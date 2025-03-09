a319ead5a0665afddf585ef90fa5fcf22963cab9
Faizan <alifaizanch3101@gmail.com>
2025-03-10 01:22:18 +0500

    not-found error set

--- app/not-found.js
+++ app/not-found.js
@@ -1,19 +1,19 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100 p-5">
-      <Image
+      {/* <Image
         src="https://i.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.webp"
         alt="Coming Soon Animation"
         width={288} // 72 * 4 = 288px
         height={288}
         priority
         className="animate-bounce"
-      />
+      /> */}
+      <div className="text-8xl text-emerald-500 font-bold text-center animate-pulse">Comming Soon!</div>
       <h1 className="text-4xl font-bold text-gray-900 mt-4">Oops! Page Coming Soon 🚀</h1>
-      <p className="text-gray-700 text-lg mt-2">We&apos;re working on this page. Stay tuned! 🔧</p>
+      <p className="text-gray-700 text-lg my-4">We&apos;re working on this page. Stay tuned! 🔧</p>
       <Link href="/">
         <span className="mt-5 px-6 py-3 bg-blue-600 text-white text-lg rounded-lg shadow-md hover:bg-blue-700 hover:scale-105 transition-all cursor-pointer">
           Go Back Home
